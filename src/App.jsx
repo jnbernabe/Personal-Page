bcd9c96baff878e5ed4e01ea13b6e09d1099dd34
Jamaal Bernabe <85321486+jnbernabe@users.noreply.github.com>
2024-12-11 16:08:30 -0500

    Updates for Improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Link,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import {
   Container,
   Row,
@@ -20,33 +14,97 @@ const githubUrl = import.meta.env.VITE_GITHUB_URL;
 const Home = () => (
   <Container className=" min-vh-100 text-left py-5">
     <Row className="w-100">
-      <Col md={8} lg={6} className="mx-auto text-center">
+      <Col className="mx-auto text-center">
         <h1 className="display-4 text-light mb-3">Jamaal Bernabe</h1>
         <h2 className="h4 text-primary mb-4">Software Developer</h2>
         <p className="text-light lead mb-4">
-          Innovating technology through scalable solutions in software
-          development, automation, and cloud integration.
+          I’m Jamaal Bernabe, a passionate software developer focused on
+          automation, full-stack development, and cloud services.
         </p>
-        <div className="d-flex justify-content-center mb-4">
-          <Button
-            href="assets/Jamaal Bernabe - Resume-DRkezgX2.pdf"
-            variant="primary"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-primary text-light"
-          >
-            Download Resume
-          </Button>
-        </div>
-        <Button
-          href={githubUrl}
-          variant="info"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-primary text-light"
-        >
-          Visit My GitHub Profile
-        </Button>
+
+        <Row>
+          <Col md={12}>
+            <h2 className="h5 text-primary mb-2">Key Skills</h2>
+            <ul className="list-unstyled text-light mb-4">
+              <li>
+                <h5>Front-end Skills</h5> HTML/CSS, JavaScript, Angular,
+                ReactJS, Selenium, JUnit, JMeter
+              </li>
+              <br />
+              <li>
+                <h5>Back-end Skills</h5> Python, C#, Java, Node.js, Next.js,
+                MongoDB, SQL, MySQL, NoSQL, ElasticSearch
+              </li>
+              <br />
+              <li>
+                <h5>Technical Skills</h5> Agile, Scrum, SDLC, CI/CD, Unit
+                Testing, Integration Testing, Database Management, Web
+                Development, JIRA, Atlassian, Jenkins, Git, Github
+              </li>
+              <br />
+              <li>
+                <h5>Machine Learning Skills</h5> NumPy, Pandas, Machine
+                Learning, Natural Language Processing, Data Visualization,
+                Keras, Sci-kit Learn, Data Modelling, Statistics
+              </li>
+            </ul>
+          </Col>
+          <Col>
+            <h2 className="h5 text-primary mb-2">Education</h2>
+            <p className="text-light mb-4">
+              Software Engineering Technology (AI) – Centennial College <br />
+              Bachelor’s in Biochemistry & Molecular Biology – University of
+              Toronto
+            </p>
+          </Col>
+          <Col>
+            <h2 className="h5 text-primary mb-2">Contact Information</h2>
+            <p className="text-light">
+              Email: jnbernabe@gmail.com <br />
+              Phone: (647) 402-1724 <br />
+              Location: Toronto, Ontario
+            </p>
+
+            <div>
+              <Button
+                href="https://linkedin.com/in/jamaalbernabe"
+                className="text-primary hover-underline text-light"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                LinkedIn Profile
+              </Button>
+            </div>
+          </Col>
+          <br />
+        </Row>
+        <br />
+        <Row>
+          <Col>
+            <div className="d-flex justify-content-center mb-4">
+              <Button
+                href="assets/Jamaal Bernabe - Resume-DRkezgX2.pdf"
+                variant="primary"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-primary text-light"
+              >
+                Download Resume
+              </Button>
+            </div>
+            <div>
+              <Button
+                href={githubUrl}
+                variant="info"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-primary text-light"
+              >
+                Visit My GitHub Profile
+              </Button>
+            </div>
+          </Col>
+        </Row>
       </Col>
     </Row>
   </Container>
@@ -107,27 +165,57 @@ const Work = () => (
       </Col>
     </Row>
     <br />
-    <div className="border-bottom border-secondary mb-4 pb-3">
-      <h2 className="h5 text-primary">Aficio AI (Feb 2024 – Jul 2024)</h2>
-      <h3 className="h6 text-light">Software Developer</h3>
-      <ul className="text-light small list-unstyled mt-2">
-        <li>Integrated Azure and AWS platforms to streamline pipelines.</li>
-        <li>Optimized Azure infrastructure for restful APIs.</li>
-        <li>Redesigned core frameworks to enhance performance.</li>
-        <li>Integrated OpenAI and Nylas APIs for added functionality.</li>
-      </ul>
-    </div>
-    <div className="border-bottom border-secondary mb-4 pb-3">
-      <h2 className="h5 text-primary">
-        Ministry of Children (Aug 2023 – Aug 2024)
-      </h2>
-      <h3 className="h6 text-light">Junior Software Automation Tester</h3>
-      <ul className="text-light small list-unstyled mt-2">
-        <li>Developed automated scripts with Selenium and JUnit.</li>
-        <li>Improved reliability by migrating to Java 17.</li>
-        <li>Reduced testing time with CI/CD pipelines.</li>
-      </ul>
-    </div>
+    <Row>
+      <div className="border-bottom border-secondary mb-4 pb-3">
+        <h2 className="h5 text-primary">
+          Aficio AI (Feburary 2024 – July 2024)
+        </h2>
+        <h3 className="h6 text-light">Software Developer</h3>
+        <ul className="text-light small list-unstyled mt-2">
+          <li>Integrated Azure and AWS platforms to streamline pipelines.</li>
+          <li>Optimized Azure infrastructure for restful APIs.</li>
+          <li>Redesigned core frameworks to enhance performance.</li>
+          <li>Integrated OpenAI and Nylas APIs for added functionality.</li>
+        </ul>
+      </div>
+    </Row>
+    <Row>
+      <div className="border-bottom border-secondary mb-4 pb-3">
+        <h2 className="h5 text-primary">
+          Ministry of Children, Community, and Social Services (August 2022 –
+          August 2023)
+        </h2>
+        <h3 className="h6 text-light">Junior Software Automation Tester</h3>
+        <ul className="text-light small list-unstyled mt-2">
+          <li>Developed automated scripts with Selenium and JUnit.</li>
+          <li>Improved reliability by migrating to Java 17.</li>
+          <li>Reduced testing time with CI/CD pipelines.</li>
+        </ul>
+      </div>
+    </Row>
+    <Row>
+      <div className="border-bottom border-secondary mb-4 pb-3">
+        <h2 className="h5 text-primary">
+          Tribunals Ontario (January 2022 – April 2022)
+        </h2>
+        <h3 className="h6 text-light">Software Developer Intern</h3>
+        <ul className="text-light small list-unstyled mt-2">
+          <li>
+            Generated detailed reports on asset status using data analysis
+            tools, improving allocation accuracy by 35%.
+          </li>
+          <li>
+            Utilized technical background to provide superb assistance to
+            adjudicators.
+          </li>
+          <li>
+            Exercised problem-solving skills in assisting staff by identifying
+            and solving root causes of issues, enhancing operational efficiency
+            and user satisfaction across the organization
+          </li>
+        </ul>
+      </div>
+    </Row>
   </Container>
 );
 
@@ -183,7 +271,7 @@ const App = () => {
         <Container>
           <Navbar.Brand
             as={Link}
-            to="/"
+            to="/Portfolio"
             className="text-light font-weight-bold"
           >
             Jamaal Bernabe
@@ -197,22 +285,22 @@ const App = () => {
               defaultActiveKey="/home"
             >
               <Nav.Item>
-                <Nav.Link as={Link} to="/" className="text-light">
+                <Nav.Link as={Link} to="/Portfolio" className="text-light">
                   Home
                 </Nav.Link>
               </Nav.Item>
+
               <Nav.Item>
-                <Nav.Link as={Link} to="/about" className="text-light">
-                  About
-                </Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link as={Link} to="/work" className="text-light">
+                <Nav.Link as={Link} to="/Portfolio/work" className="text-light">
                   Work
                 </Nav.Link>
               </Nav.Item>
               <Nav.Item>
-                <Nav.Link as={Link} to="/projects" className="text-light">
+                <Nav.Link
+                  as={Link}
+                  to="/Portfolio/projects"
+                  className="text-light"
+                >
                   Projects
                 </Nav.Link>
               </Nav.Item>
@@ -222,11 +310,9 @@ const App = () => {
       </Navbar>
       <Container fluid className="bg-dark text-light p-0 min-vh-100">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/work" element={<Work />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="*" element={<Navigate to="/" />} />
+          <Route path="/Portfolio" element={<Home />} />
+          <Route path="/Portfolio/work" element={<Work />} />
+          <Route path="/Portfolio/projects" element={<Projects />} />
         </Routes>
       </Container>
       <footer className="bg-secondary text-center py-3">
